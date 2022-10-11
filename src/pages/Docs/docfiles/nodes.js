ea822f1e31ddbc45bf903a6eab696616dfc18be6
papaSneetch <colin.bernhardt3@gmail.com>
2022-10-11 01:48:51 -0600

    Added information on bootstrap, slurm and apache.

--- src/pages/Docs/docfiles/nodes.js
+++ src/pages/Docs/docfiles/nodes.js
@@ -20,6 +20,24 @@ const nodes = () => {
         SLURM also needs to be up and running on the node. Ideally, the node gets connected to Pilot so you can check uptime as well.
       </p>
       
+      <p>
+        You can find documentation for the system used on the Advanced Clustering website. There is a Apex Clustering manual at, 
+        <a href="https://www.advancedclustering.com/wp-content/uploads/2017/02/Apex_Cluster_Manual-2.2-1.pdf">https://www.advancedclustering.com/wp-content/uploads/2017/02/Apex_Cluster_Manual-2.2-1.pdf</a>. 
+        It is also important to know how to restart slurm if it ever goes down. You can start the slurm controller on the head node with, <br></br><br></br>
+        <code>systemctl restart slurmctld</code>.
+
+        You can restart slurm on the compute node with<br></br><br></br> <code>systemctl restart slurmd</code>.<br></br><br></br>
+      
+      Next, you can find additional documentation for slurm at, <a href="https://slurm.schedmd.com/quickstart_admin.html">
+      https://slurm.schedmd.com/quickstart_admin.html</a>.
+      </p>
+      <p>
+        Next, it's also important to learn that our Websites use Apache for its webserver. If adding a new tool to the site,
+        it's important to make sure that apache has been modified to recognize the new tool.
+      </p>
+      <p>
+        Finally, the web pages use bootstrap for it's base for it's css. You can find additional documentation on it here, <a href="https://getbootstrap.com/docs/4.1/getting-started/introduction/">https://getbootstrap.com/docs/4.1/getting-started/introduction/</a>.
+      </p>
     </div>
   );
 }
