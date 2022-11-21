d506d5e8bbc52a98d7a699628fa50deb3d20102b
papaSneetch <a02308059@usu.edu>
2022-11-21 16:23:56 -0700

    Added additional apache information.

--- src/pages/Docs/docfiles/slurm.js
+++ src/pages/Docs/docfiles/slurm.js
@@ -13,7 +13,12 @@ const slurm = () => {
       </p>
       <p>
         Next, it's also important to learn that our Websites use Apache for its webserver. If adding a new tool to the site,
-        it's important to make sure that apache has been modified to recognize the new tool.
+        it's important to make sure that apache has been modified to recognize the new tool. The config files are located at "/etc/httpd/conf". Make sure that after editing the file
+        you do a config test with <br></br><br></br><code>apachectl configtest</code><br></br><br></br>
+
+        Afterwards you can restart the service with, <br></br><br></br>
+        <code>sudo systemctl restart apache2.service</code>
+        <br></br><br></br>
       </p>
       <p>
         Finally, the web pages use bootstrap for it's base for it's css. You can find additional documentation on it here, <a href="https://getbootstrap.com/docs/4.1/getting-started/introduction/">https://getbootstrap.com/docs/4.1/getting-started/introduction/</a>.
